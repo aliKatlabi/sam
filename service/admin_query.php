a97e0eaafb26e0a445628054dc989af1b2f87241
alikatlabi <a2222k@gmail.com>
2020-03-16 00:13:28 +0100

    admin_query.php

---
+++ service/admin_query.php
@@ -0,0 +1,26 @@
+<?php
+
+include 'utiles.php';
+
+
+//  - Query Table button functionality 
+
+//  - Check if the Grade in specific row exist (not -1) 
+// 		- if yes : Query all information in the row 
+//		- if no  : Query only (IDCode , file , Date , Time)
+//	- 
+//	- 
+
+
+
+/* ID Code
+Neptun Code
+Student Name
+Submitted File
+Report
+Date
+Time   */
+
+
+
+?>
